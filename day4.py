bd2d2976ea106bb7d75d276525db1ae788bd1a7d
Gordon Shieh <gshieh@sfu.ca>
2017-12-22 01:00:44 -0800

    Add part 2 solution for day 4

--- day4.py
+++ day4.py
@@ -1,7 +1,15 @@
-
 def is_valid(line):
     return len(line.split()) == len(frozenset(line.split()))
-    
+
+
+def has_anagram(line):
+    words = line.split()
+    letter_sets = [frozenset(item) for item in words]
+    return len(frozenset(letter_sets)) != len(letter_sets)
+
+
 with open('day4_input.txt', 'r') as input:
     print(sum(is_valid(line) for line in input))
-    
+
+with open('day4_input.txt', 'r') as input:
+    print(sum(not has_anagram(line) for line in input))
